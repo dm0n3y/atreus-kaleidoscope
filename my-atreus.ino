102c2d7e0a96b68f07bcd19a2a947e030d5e4874
David Moon <dmoon1221@gmail.com>
2024-03-04 19:30:28 -0500

    disable extra keys

--- my-atreus.ino
+++ my-atreus.ino
@@ -90,15 +90,15 @@ enum { BASE, NAV, SYM, NUM };
 KEYMAPS(
   [BASE] = KEYMAP_STACKED
   (
-       Key_Q    ,Key_W     ,Key_E     ,Key_R          ,Key_T
-      ,Key_A    ,Key_S     ,Key_D     ,Key_F          ,Key_G
-      ,Key_Z    ,Key_X     ,Key_C     ,Key_V          ,Key_B         ,Key_PlayPause
-      ,Key_Mute ,Key_VolDn ,Key_VolUp ,LT(NUM,Escape) ,LT(NAV,Space) ,SFT_T(Tab)
+       Key_Q ,Key_W ,Key_E ,Key_R          ,Key_T
+      ,Key_A ,Key_S ,Key_D ,Key_F          ,Key_G
+      ,Key_Z ,Key_X ,Key_C ,Key_V          ,Key_B         ,___
+      ,___   ,___   ,___   ,LT(NUM,Escape) ,LT(NAV,Space) ,SFT_T(Tab)
 
-                      ,Key_Y             ,Key_U      ,Key_I        ,Key_O        ,Key_P
-                      ,Key_H             ,Key_J      ,Key_K        ,Key_L        ,Key_Semicolon
-       ,Key_NextTrack ,Key_N             ,Key_M      ,Key_Comma    ,Key_Period   ,Key_Question
-       ,SFT_T(Enter)  ,LT(SYM,Backspace) ,Key_Delete ,Key_BrightUp ,Key_BrightDn ,Key_Sleep
+                     ,Key_Y             ,Key_U      ,Key_I     ,Key_O      ,Key_P
+                     ,Key_H             ,Key_J      ,Key_K     ,Key_L      ,Key_Semicolon
+       ,___          ,Key_N             ,Key_M      ,Key_Comma ,Key_Period ,Key_Question
+       ,SFT_T(Enter) ,LT(SYM,Backspace) ,Key_Delete ,___       ,___        ,___
   ),
 
   [NAV] = KEYMAP_STACKED
